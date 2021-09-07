b7645dfd0f5b6519459b1ff79bf61eb1321226b9
Devin Austin <devin.austin@gmail.com>
2021-09-07 17:00:39 -0600

    cleanup and refactor of ImageHandler stuff, implemented FileStore and FileImage, consolidated code for tests and moved common functionality to its respective trait. this closes #4

--- src/main/scala/in/stonecolddev/dali/ImageHandler.scala
+++ src/main/scala/in/stonecolddev/dali/ImageHandler.scala
@@ -1,28 +1,66 @@
 package in.stonecolddev.dali
 
+
+
 import java.awt.image.BufferedImage
 
 object ImageHandler {
 
   type ImageLocation = String
+  type MimeType = String
 
   type ResizeStrategy = (Int, Int, BufferedImage) => BufferedImage
 
-  type Store = BufferedImage => Unit
-
-  type Read = ImageLocation => BufferedImage
+  type Writer = (ImageLocation, MimeType, BufferedImage) => Unit
+  type Reader = ImageLocation => BufferedImage
 
   trait Resizer {
     def resize: ResizeStrategy
   }
 
-  trait Image {
+  trait Image extends Store {
     val location: ImageLocation
     val height: Int
     val width: Int
     val name: String
     val description: String
-    def read: Read
-    def store: Store
+    val mimeType: String
+    val data: BufferedImage
+  }
+
+  trait Store {
+    def reader: Reader
+    def writer: Writer
+  }
+
+  object FileStore {
+
+    import java.io.File
+    import javax.imageio.ImageIO
+
+    trait FileStore extends Store {
+      def reader: Reader =
+        (imageLocation: String) => ImageIO.read(new File(imageLocation))
+
+      def writer: Writer = {
+        (imageLocation, mimeType, bufferedImage: BufferedImage) =>
+          val file = new File(imageLocation)
+          // TODO: make this less shitty
+          new File(file.getParent).mkdirs()
+          ImageIO.write(bufferedImage, mimeType.split("/")(1), file)
+      }
+    }
+
+    case class FileImage(
+      location: ImageLocation, // TODO: generate a slug here
+      height: Int,
+      width: Int,
+      name: String,
+      description: String,
+      mimeType: String,
+      data: BufferedImage) extends Image with FileStore {
+        def write(): Unit = writer(location, mimeType, data)
+        def read(): BufferedImage = reader(location)
+    }
   }
 }
--- src/test/scala/in/stonecolddev/dali/ImageHandlerTest.scala
+++ src/test/scala/in/stonecolddev/dali/ImageHandlerTest.scala
@@ -1,6 +1,8 @@
 package in.stonecolddev.dali
 
-import in.stonecolddev.dali.ImageHandler.{ImageLocation, Image, Read, Resizer, Store}
+import in.stonecolddev.dali.ImageHandler.FileStore.FileImage
+import in.stonecolddev.dali.ImageHandler._
+import org.scalamock.function.{MockFunction1, MockFunction3}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
@@ -9,50 +11,89 @@ import java.awt.image.BufferedImage
 
 class ImageHandlerTest extends AnyFlatSpec with should.Matchers with MockFactory {
 
-  "ImageHandler.Resizer" should "resize an image" in new MockImageTest {
-    val resizeStrategy = new Resizer {
-      override def resize = (width: Int, height: Int, buffered: BufferedImage) => {
-        fakeBufferedImage(width, height)
+  "ImageHandler.Resizer" should "call resize correctly" in new MockImageTest {
+    val resizeStrategy: Resizer = new Resizer {
+      override def resize: ResizeStrategy = (width: Int, height: Int, _: BufferedImage) => {
+        inMemoryBufferedImage(width, height)
       }
     }
 
     bufferedImg.getHeight should equal(250)
     bufferedImg.getWidth should equal(250)
 
-    val r = resizeStrategy.resize(150, 150, bufferedImg)
+    val r: BufferedImage = resizeStrategy.resize(150, 150, bufferedImg)
     r.getHeight should equal(150)
     r.getWidth should equal(150)
   }
 
-  "Image" should "read and store an image" in new MockImageTest {
+  "ImageHandler.Image" should "call read and store correctly" in new MockImageTest {
     mockRead expects imageLocation returns bufferedImg
-    mockStore expects bufferedImg
+    mockStore expects(imageLocation, mimeType, bufferedImg)
 
-    MockImage.read(imageLocation) shouldBe bufferedImg
-    MockImage.store(bufferedImg)
+    MockImage.reader(imageLocation) shouldBe bufferedImg
+    MockImage.writer(imageLocation, mimeType, bufferedImg)
   }
 
-  trait MockImageTest {
+  "FileStore.FileImage" should "store an image correctly" in new FileImageTest {
+    // TODO move this to FileImageTest
+    try {
+      fi.write()
 
-    val bufferedImg = fakeBufferedImage()
-    val imageLocation = "/stupid/dumb/path"
-    val mockRead = mockFunction[ImageLocation, BufferedImage]
-    val mockStore = mockFunction[BufferedImage, Unit]
-
-
-    object MockImage extends Image {
-      override val location: ImageLocation = imageLocation
-      override val height: Int = 250
-      override val width: Int = 250
-      override val name: String = "fart"
-      override val description: String = "a fart."
-      override val read: Read = mockRead
-      override val store: Store = mockStore
+      val actual = readTestImage()
+      actual.getHeight shouldBe bufferedImg.getHeight
+      actual.getWidth shouldBe bufferedImg.getWidth
+    } finally {
+      cleanup()
     }
+  }
 
-    private def fakeBufferedImage(): BufferedImage = fakeBufferedImage(250, 250)
+  it should "read and image correctly" in new FileImageTest {
+    try {
+      writeTestImage()
 
-    def fakeBufferedImage(width: Int, height: Int) = {
+      val actual = fi.read()
+      actual.getHeight shouldBe bufferedImg.getHeight
+      actual.getWidth shouldBe bufferedImg.getWidth
+    } finally {
+      cleanup()
+    }
+  }
+
+  trait FileImageTest extends ImageTest {
+    import java.io.File
+    import javax.imageio.ImageIO
+
+    override val name = "file store image name"
+    override val description = "file store image description"
+
+    private lazy val imageFile = new File(imageLocation)
+
+    lazy val fi: FileImage =
+      FileImage(imageLocation, bufferedImg.getHeight(), bufferedImg.getWidth(), name, description, mimeType, bufferedImg)
+
+    def readTestImage(): BufferedImage = ImageIO.read(imageFile)
+    def writeTestImage(): Unit =
+      ImageIO.write(bufferedImg, format, imageFile)
+
+
+    def cleanup(): Boolean = {
+      import scala.reflect.io.Directory
+      new Directory(new File(imageLocation)).deleteRecursively()
+    }
+  }
+
+  trait ImageTest {
+    val bufferedImg: BufferedImage = inMemoryBufferedImage()
+    val name: String
+    val description: String
+    val mimeType = "image/jpg"
+    val format: MimeType = mimeType.split("/")(1)
+    lazy val imageLocation = s"/tmp/dali/images/${name.split("\\s").mkString("+")}.${format}"
+
+
+    def inMemoryBufferedImage(): BufferedImage = inMemoryBufferedImage(250, 250)
+
+    def inMemoryBufferedImage(width: Int, height: Int): BufferedImage = {
       import java.awt.Graphics
       import java.awt.image.BufferedImage
       val bufferedImage: BufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
@@ -61,6 +102,28 @@ class ImageHandlerTest extends AnyFlatSpec with should.Matchers with MockFactory
       g.drawString("dali", 20, 20)
 
       bufferedImage
+    }
+  }
+
+  trait MockImageTest extends ImageTest {
+
+    val mockRead: MockFunction1[ImageLocation, BufferedImage] = mockFunction[ImageLocation, BufferedImage]
+    val mockStore: MockFunction3[ImageLocation, MimeType, BufferedImage, Unit] =
+      mockFunction[ImageLocation, MimeType, BufferedImage, Unit]
+
+    override val name = "mock image name"
+    override val description = "mock image description"
+
+    object MockImage extends Image {
+      override val location: ImageLocation = imageLocation
+      override val height: Int = 250
+      override val width: Int = 250
+      override val name: String = name
+      override val description: String = description
+      override val reader: Reader = mockRead
+      override val writer: Writer = mockStore
+      override val mimeType: String = mimeType
+      override val data: BufferedImage = inMemoryBufferedImage()
     }
   }
 }
